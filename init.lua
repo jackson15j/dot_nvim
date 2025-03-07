67f349201b8320000cec6aea2b05dc8352174bdb
Craig Astill <jackson15j@hotmail.com>
2025-03-07 12:19:19 +0000

    refactor: config into which-key plugin block.

--- init.lua
+++ init.lua
@@ -276,24 +276,51 @@ require("lazy").setup({
   "romgrk/barbar.nvim",
 },
 
-{
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
+  {
+    "folke/which-key.nvim",
+    config = function()
+      require("which-key").add({
+        { "<leader>f", group = "file" }, -- group
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+        { "<leader>fb", function() print("hello") end, desc = "Foobar" },
+        { "<leader>fn", desc = "New File" },
+        { "<leader>f1", hidden = true }, -- hide this keymap
+        { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+        { "<leader>b", group = "buffers", expand = function()
+            return require("which-key.extras").expand.buf()
+          end
+        },
+        {
+          -- Nested mappings are allowed and can be added in any order
+          -- Most attributes can be inherited or overridden on any level
+          -- There's no limit to the depth of nesting
+          mode = { "n", "v" }, -- NORMAL and VISUAL mode
+          { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+          { "<leader>s", "<cmd>w<cr>", desc = "Save" },
+        }
+      })
+      -- -- Show hydra mode for changing windows
+      -- wk.show({
+        -- keys = "<c-w>",
+        -- loop = true, -- this will keep the popup open until you hit <esc>
+      -- })
+    end,
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
     },
   },
-},
 
 
   {
@@ -329,29 +356,4 @@ require("lazy").setup({
 require("devcontainer").setup{}
 require("barbar").setup()
 
-local wk = require("which-key")
-wk.add({
-  { "<leader>f", group = "file" }, -- group
-  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
-  { "<leader>fb", function() print("hello") end, desc = "Foobar" },
-  { "<leader>fn", desc = "New File" },
-  { "<leader>f1", hidden = true }, -- hide this keymap
-  { "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
-  { "<leader>b", group = "buffers", expand = function()
-      return require("which-key.extras").expand.buf()
-    end
-  },
-  {
-    -- Nested mappings are allowed and can be added in any order
-    -- Most attributes can be inherited or overridden on any level
-    -- There's no limit to the depth of nesting
-    mode = { "n", "v" }, -- NORMAL and VISUAL mode
-    { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
-    { "<leader>s", "<cmd>w<cr>", desc = "Save" },
-  }
-})
--- -- Show hydra mode for changing windows
--- wk.show({
-  -- keys = "<c-w>",
-  -- loop = true, -- this will keep the popup open until you hit <esc>
--- })
+
